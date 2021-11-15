9b1a06e7e1a6cf9f38d3927288c320a5577eee14
Zhakhangir <carrymartes@gmail.com>
2021-11-16 01:08:24 +0600

    index unused files removed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Main from '@/pages/Main/index.vue'
-import Stalls from '@/pages/Main/Stalls/index.vue'
-import Animals from '@/pages/Main/Animals/index.vue'
-import Calendar from '@/pages/Main/Calendar/index.vue'
-import Analytics from '@/pages/Main/Analytics/index.vue'
-import Settings from '@/pages/Main/Settings/index.vue'
-import Users from '@/pages/Main/Users/index.vue'
-import Operations from '@/pages/Main/Operations/index.vue'
-import animal_groups from '@/pages/Main/Animals/categories/groups.vue'
-import animal_lists from '@/pages/Main/Animals/categories/lists.vue'
 import authRoute from './auth/'
 import homeRoutes from './home'
 
@@ -20,11 +10,11 @@ const routes = [
     ...authRoute,
   },
   {
-    path: '/',
-    redirect: '/auth/login',
+    ...homeRoutes,
   },
   {
-    ...homeRoutes,
+    path: '/',
+    redirect: '/auth/login',
   },
 ]
 
