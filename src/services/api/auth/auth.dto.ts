e1778a1f8c6b7fb32ff4f147b5539845508571c3
Zhakhangir <carrymartes@gmail.com>
2021-11-16 12:33:42 +0600

    auth path requests changed and added new SignUp, Refresh token

--- src/services/api/auth/auth.dto.ts
+++ src/services/api/auth/auth.dto.ts
@@ -1,8 +1,19 @@
 export type DTOReqLogin = {
-  login: string
+  email: string
   password: string
 }
 export type DTOResLogin = {
   login: string
   password: string
+}
+export type DTOReqSignUp = {
+  first_name: string
+  last_name: string
+  email: string
+  password: string
+  phone_number: number
+  organization_name: string
+}
+export type DTORefreshToken = {
+  token: string
 }
--- src/services/api/auth/auth.service.ts
+++ src/services/api/auth/auth.service.ts
@@ -1,12 +1,19 @@
 import BaseApi from '@/services/api/base.api'
-import { DTOReqLogin, DTOResLogin } from '@/services/api/auth/auth.dto'
+import {
+  DTOReqLogin,
+  DTOReqSignUp,
+  DTOResLogin,
+  DTORefreshToken,
+} from '@/services/api/auth/auth.dto'
 
 enum apiEndpoints {
   login = 'login',
+  registration = 'users/sign-up',
+  refreshToken = 'refresh-token',
 }
 
 const BACKEND_URL = process.env.VUE_APP_CORE_API_BASE_URL
-const API_VERSION = '/api/v3'
+const API_VERSION = '/api/auth'
 
 class authService extends BaseApi {
   private endpoints = apiEndpoints
@@ -19,6 +26,14 @@ class authService extends BaseApi {
 
   login(data: DTOReqLogin) {
     return this.post<DTOResLogin>(this.endpoints.login, data)
+  }
+
+  signUp(data: DTOReqSignUp) {
+    return this.post<DTOReqSignUp>(this.endpoints.registration, data)
+  }
+
+  refreshToken(data: DTORefreshToken) {
+    return this.post<DTORefreshToken>(this.endpoints.refreshToken, data)
   }
 }
 
