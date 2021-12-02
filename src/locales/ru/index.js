ef3342d8fbc5998bf0677c9256fcf77d1e9265cd
f1eeman <mr.f1eeman@gmail.com>
2021-12-03 01:47:02 +0600

    feat(locales): Добавлены переводы для таба "ВОЙТИ"

--- src/locales/ru/index.js
+++ src/locales/ru/index.js
@@ -3,9 +3,11 @@ import home from './home.json'
 import auth from './auth.json'
 import loading from './loading.json'
 import errors from './errors.json'
+import login from './login.json'
 import breadcrumbs from './breadcrumps.json'
 
 export default {
+  login,
   auth,
   general,
   home,
