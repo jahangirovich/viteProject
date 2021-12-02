ae1805d50daea0d6f5079d8b9fc12a29d3765982
f1eeman <mr.f1eeman@gmail.com>
2021-12-03 01:49:49 +0600

    fix(layout): Исправлена мобильная и десктопная версии

--- src/pages/Auth/index.vue
+++ src/pages/Auth/index.vue
@@ -1,144 +1,83 @@
 <template>
   <v-app>
-    <v-main class="auth main">
-      <v-container fluid class="main_mobile-block accent">
-        <div class="p-0 d-flex flex-column">
-          <div class="main_auth-header main_header text-center my-7">
+    <v-main class="auth main accent">
+      <v-container fluid class="px-0 py-0">
+        <div class="content-list">
+          <div class="content-item text-center pt-6">
             <v-img
               :src="require('@/assets/img/auth/cowmas_logo.svg')"
               width="141px"
               height="74px"
-              class="ma-auto"
+              class="ma-auto mb-4"
             >
             </v-img>
             <div
-              class="font-weight-light white--text text-center mt-5"
-              v-html="i18n.t('auth.header_description')"
+              class="logo-text font-weight-regular white--text text-center mb-4"
+              v-html="$t('auth.header_description')"
             ></div>
           </div>
-          <v-card class="rounded-lg shadow-md overflow-hidden main_body">
-            <v-tabs v-model="model" centered fixed-tabs>
-              <v-tabs-slider class="accent"></v-tabs-slider>
-              <v-tab
-                v-for="(item, i) in items"
-                :key="i"
-                :to="item.link"
-                :class="
-                  $route.name === item.link_name ? 'accent--text' : 'gray_secondary--text'
-                "
-              >
-                <span class="font-weight-bold">{{ item.name }}</span>
-              </v-tab>
-            </v-tabs>
-            <v-tabs-items v-model="model">
-              <v-tab-item v-for="(item, i) in items" :key="i" :value="item.link">
-                <router-view :key="$route.path"></router-view>
-              </v-tab-item>
-            </v-tabs-items>
-          </v-card>
-          <div class="main_footer align-content-end text-center">
+          <div class="content-item" v-show="$vuetify.breakpoint.mdAndUp">
+            <v-img :src="require('@/assets/img/auth/cowmasBanner.png')"></v-img>
+          </div>
+          <div class="content-item">
+            <v-card class="rounded-lg shadow-md overflow-hidden">
+              <v-tabs height="auto" v-model="model" centered fixed-tabs>
+                <v-tabs-slider class="accent"></v-tabs-slider>
+                <v-tab
+                  v-for="(item, i) in items"
+                  :key="i"
+                  :to="item.link"
+                  class="tab-link pt-9 pb-3"
+                  :class="
+                    $route.name === item.link_name
+                      ? 'accent--text'
+                      : 'gray_secondary--text'
+                  "
+                >
+                  <span class="router-link-text">{{ item.name }}</span>
+                </v-tab>
+              </v-tabs>
+              <v-tabs-items v-model="model">
+                <v-tab-item v-for="(item, i) in items" :key="i" :value="item.link">
+                  <router-view :key="$route.path"></router-view>
+                </v-tab-item>
+              </v-tabs-items>
+            </v-card>
+          </div>
+          <div
+            class="
+              d-flex
+              flex-column
+              justify-start
+              align-center
+              pt-10
+              content-item
+              text-center
+            "
+          >
             <h2
-              class="font-weight-bold white--text text-body-2 uppercase mt-9 mb-1"
-              v-text="i18n.t('auth.footer_description')"
+              class="mb-1 pb-3 text-uppercase footer-title white--text uppercase"
+              v-text="$t('auth.footer_title')"
             ></h2>
             <a
-              class="phone my-1 text-body-2"
-              :href="i18n.t('auth.footer_phone')"
-              v-text="i18n.t('auth.footer_phone')"
+              class="footer-link y-1 white--text"
+              :href="$t('auth.footer_phone')"
+              v-text="$t('auth.footer_phone')"
             ></a>
             <a
-              class="phone text-decoration-underline my-1 text-body-2"
-              :href="i18n.t('auth.footer_support_site')"
-              v-text="i18n.t('auth.footer_support_site')"
+              class="footer-link text-decoration-underline white--text"
+              :href="$t('auth.footer_support_site')"
+              v-text="$t('auth.footer_support_site')"
             >
             </a>
           </div>
         </div>
       </v-container>
-      <!-- <div class="main_desktop-block">
-        <div class="d-flex">
-          <div
-            class="
-              main_desktop-block_leftSide
-              accent
-              px-12
-              pb-6
-              d-flex
-              flex-column
-              justify-space-between
-            "
-          >
-            <div class="text-center my-7">
-              <v-img
-                :src="require('@/assets/img/auth/cowmas_logo.svg')"
-                alt="Cowmas"
-                class="logo"
-                width="305px"
-                height="160px"
-              >
-              </v-img>
-              <div class="font-weight-light white--text text-left mt-5">
-                Платформа для мониторинга и аналитики данных в животноводстве
-              </div>
-            </div>
-            <div>
-              <v-img :src="require('@/assets/img/auth/cowmasBanner.png')"></v-img>
-            </div>
-            <div class="main_footer align-content-end text-left">
-              <h2 class="font-weight-bold text-body-2 white--text uppercase">
-                Техническая поддержка<br />
-                -
-              </h2>
-              <div class="phone text-body-2 my-1">+7 (707) 902-34-56</div>
-              <div class="phone text-body-2 text-decoration-underline">
-                support.cowmas.com
-              </div>
-            </div>
-          </div>
-          <div class="main_desktop-block_rightSide justify-center d-flex">
-            <div>
-              <v-card
-                class="
-                  main_desktop-block_rightSide_content
-                  rounded-lg
-                  shadow-md
-                  overflow-hidden
-                  main_body
-                  mt-16
-                "
-                elevation="3"
-              >
-                <v-tabs v-model="model" centered fixed-tabs>
-                  <v-tabs-slider class="accent"></v-tabs-slider>
-                  <v-tab
-                    v-for="(item, i) in items"
-                    :key="i"
-                    :to="item.link"
-                    :class="
-                      $route.name === item.link_name
-                        ? 'accent--text'
-                        : 'gray_secondary--text'
-                    "
-                  >
-                    <span class="font-weight-bold">{{ item.name }}</span>
-                  </v-tab>
-                </v-tabs>
-                <v-tabs-items v-model="model">
-                  <v-tab-item v-for="(item, i) in items" :key="i" :value="item.link">
-                    <router-view :key="$route.path"></router-view>
-                  </v-tab-item>
-                </v-tabs-items>
-              </v-card>
-            </div>
-          </div>
-        </div>
-      </div> -->
     </v-main>
   </v-app>
 </template>
 <script>
 import { defineComponent, ref } from '@vue/composition-api'
-import i18n from '@/i18n'
 
 export default defineComponent({
   setup() {
@@ -160,13 +99,83 @@ export default defineComponent({
   data() {
     return {
       model: this.$route.path.name,
-      i18n,
     }
   },
 })
 </script>
 
 <style lang="scss" scoped>
+@import '~vuetify/src/styles/styles.sass';
 @import '../../styles/variables';
-@import '../../styles/auth';
+
+.logo-text {
+  font-size: $secondary-font-size;
+  line-height: $secondary-line-height;
+}
+
+.content-list {
+  display: grid;
+  grid-template-rows: minmax(225px, max-content) minmax(360px, max-content) minmax(
+      146px,
+      max-content
+    );
+  min-height: 100vh;
+}
+
+.router-link-text {
+  font-size: $main-font-size;
+  line-height: $main-line-height;
+  font-weight: map-get($font-weights, 'semi-bold');
+}
+
+.footer-title {
+  position: relative;
+  font-size: $main-font-size;
+  line-height: $main-line-height;
+  font-weight: map-get($font-weights, 'semi-bold');
+
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    margin-left: -4px;
+    display: block;
+    width: 8px;
+    height: 2px;
+    background-color: map-get($theme-colors, 'white');
+  }
+}
+
+.footer-link {
+  font-size: $main-font-size;
+  line-height: $secondary-line-height;
+  font-weight: map-get($font-weights, 'regular');
+  opacity: 1;
+  transition: opacity 0.2s;
+
+  &:hover,
+  &:focus {
+    opacity: 0.6;
+  }
+
+  &:active {
+    opacity: 0.3;
+  }
+}
+
+@media #{map-get($display-breakpoints, 'md-and-up')} {
+  .content-list {
+    grid-template-columns: 38% 1fr;
+    grid-template-rows: 290px 1fr auto;
+  }
+
+  .content-item:nth-child(3) {
+    align-self: stretch;
+    grid-column: 2/-1;
+    grid-row: 1/-1;
+
+    background-color: map-get($theme-colors, 'white');
+  }
+}
 </style>
