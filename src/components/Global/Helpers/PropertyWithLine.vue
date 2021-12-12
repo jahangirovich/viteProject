51211f8a6a0dc7aafd4b259607f70d359ea7f8b4
Zhakhangir <carrymartes@gmail.com>
2021-12-12 16:48:58 +0600

    feature: changed profile design in animal page

---
+++ src/components/Global/Helpers/PropertyWithLine.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="(item, i) in values" :key="i">
+      <div class="d-flex py-3">
+        <v-row>
+          <v-col md="3">
+            <span class="text-body-2">{{ item.type }}</span>
+          </v-col>
+          <v-col>
+            <span class="font-weight-bold text-body-2">{{ item.value }}</span>
+          </v-col>
+        </v-row>
+      </div>
+      <v-divider v-if="i != values.length - 1"></v-divider>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: ['values'],
+  name: 'PropertyWithLine',
+}
+</script>
--- src/pages/Main/Animals/details/categories/profile.vue
+++ src/pages/Main/Animals/details/categories/profile.vue
@@ -1,112 +1,90 @@
 <template>
   <div class="d-flex py-2">
-    <v-card class="mx-3 pa-4 flex-grow-0" elevation="1">
-      <div>
-        <v-img :src="require('@/assets/main/other/cow.png')" width="232px" cover></v-img>
-        <h4 class="text-h6 font-weight-bold mt-2">{{ $t('Кличка') }}</h4>
-        <div class="mt-1">
-          <StatusBadge :status="'На ферме'" />
+    <div class="flex-grow-0">
+      <v-card class="mx-3 pa-4" elevation="1">
+        <div>
+          <v-img
+            :src="require('@/assets/main/other/cow.png')"
+            width="232px"
+            cover
+          ></v-img>
+          <h4 class="text-h6 font-weight-bold mt-2">{{ $t('Кличка') }}</h4>
+          <div class="mt-1">
+            <StatusBadge :status="'На ферме'" />
+          </div>
+          <div class="mt-3" v-for="(cow, i) in cowInfos" :key="i">
+            <LabelWithInfo :type="cow.type" :value="cow.value" />
+          </div>
+          <v-divider class="my-2"></v-divider>
+          <div class="mt-2 d-flex justify-space-between">
+            <div class="d-flex align-center">
+              <div>
+                <v-img
+                  :src="require('@/assets/main/animals/profile/cowIcon.svg')"
+                  width="19px"
+                  height="19px"
+                />
+              </div>
+              <span class="text-body-2 font-weight-bold">4.6лет</span>
+            </div>
+            <div class="d-flex align-center">
+              <div>
+                <v-img
+                  :src="require('@/assets/main/animals/profile/weightIcon.svg')"
+                  width="19px"
+                  height="19px"
+                />
+              </div>
+              <span class="text-body-2 font-weight-bold ml-1">44 кг</span>
+            </div>
+            <div class="d-flex align-center">
+              <div>
+                <v-img
+                  :src="require('@/assets/main/animals/profile/topBottomIcon.svg')"
+                  width="19px"
+                  height="19px"
+                />
+              </div>
+              <span class="text-body-2 font-weight-bold">150 см</span>
+            </div>
+          </div>
         </div>
-        <div class="mt-3">
-          <LabelWithInfo type="Тип" value="Теленок" />
-        </div>
-        <div class="mt-3">
-          <LabelWithInfo type="Порода" value="Текст" />
-        </div>
-        <div class="mt-3">
-          <LabelWithInfo type="Пол" value="Самка" />
-        </div>
-        <div class="mt-3">
-          <LabelWithInfo type="Происхождение" value="Рожден на ферме" />
-        </div>
-        <div class="mt-3">
-          <LabelWithInfo type="Дата рождения" value="17.06.2021" />
-        </div>
-        <div class="mt-3">
-          <LabelWithInfo type="Вес" value="44 кг" />
-        </div>
-      </div>
-    </v-card>
-    <v-divider vertical></v-divider>
-    <div class="mx-3 flex-grow-0">
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo type="ID" value="123456789" />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo type="Локальный ID" value="123456789" />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo type="Номер RFID" value="123456789" />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo type="Номер тавро" value="123456789" />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo
-          type="Отец"
-          :link="{ name: '123456789', href: 'home/animals/123456789' }"
-        />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo
-          type="Мать"
-          :link="{ name: '123456789', href: 'home/animals/123456789' }"
-        />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo type="Отъем от кормления" value="17.06.2021" />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo
-          type="Группа"
-          :link="{ name: 'Название группы', href: 'home/animals/123456789' }"
-        />
-      </v-card>
-      <v-card elevation="1" width="220px" class="pa-3 mb-2">
-        <LabelWithInfo
-          type="Стойла"
-          :link="{ name: 'Название стойла', href: 'home/animals/123456789' }"
-        />
       </v-card>
     </div>
     <v-divider vertical></v-divider>
     <div class="mx-3 flex-grow-1" height="auto" elevation="1">
-      <v-card>
-        <h4 class="text-body-2 font-weight-bold pa-4">{{ $t('Кличка') }}</h4>
-        <v-divider></v-divider>
-        <div class="content pa-4">
-          <div class="mb-4">
-            <span>04.10.2021</span>
-            <v-btn depressed class="text-none text-body-2 ml-4 rounded-lg">
-              <v-avatar color="green" size="14" class="mr-3">
-                <span class="white--text text-h5"></span>
-              </v-avatar>
-              {{ $t('Наименование события') }}
-            </v-btn>
-          </div>
-          <div class="mb-4">
-            <span>04.10.2021</span>
-            <v-btn depressed class="text-none text-body-2 ml-4 rounded-lg">
-              <v-avatar color="accent" size="14" class="mr-3">
-                <span class="white--text text-h5"></span>
-              </v-avatar>
-              {{ $t('Наименование события') }}
-            </v-btn>
-          </div>
-          <div class="mb-4">
-            <span>04.10.2021</span>
-            <v-btn depressed class="text-none text-body-2 ml-4 rounded-lg">
-              <v-avatar color="accent" size="14" class="mr-3">
-                <span class="white--text text-h5"></span>
-              </v-avatar>
-              {{ $t('Наименование события') }}
-            </v-btn>
-          </div>
-        </div>
-      </v-card>
+      <v-expansion-panels multiple accordion flat v-model="expandedPanels">
+        <v-expansion-panel
+          class="mb-4 elevation-0 rounded-lg"
+          v-for="(item, i) in panels"
+          :key="i"
+        >
+          <v-expansion-panel-header class="px-4">
+            <h4 class="text-body-2 font-weight-bold">{{ $t(item.title) }}</h4>
+          </v-expansion-panel-header>
+          <v-divider v-if="panels.includes(i)"></v-divider>
+          <v-expansion-panel-content class="mt-2">
+            <PropertyWithLine :values="item.items" />
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </div>
   </div>
 </template>
+
+<script>
+import { ref } from '@vue/composition-api'
+import { panelItems, cowInfo } from './index'
+export default {
+  setup() {
+    const panels = ref(panelItems)
+    const cowInfos = ref(cowInfo)
+    // setting opened panels
+    const expandedPanels = ref(Array.from({ length: panelItems.length }, (v, k) => k))
+    return { expandedPanels, panels, cowInfos }
+  },
+}
+</script>
 
 <style lang="scss">
 img {
