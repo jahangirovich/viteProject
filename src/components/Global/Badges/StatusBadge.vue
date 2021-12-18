e92116f97be47c64caeb76a63bb24776b0ec2932
Zhakhangir <carrymartes@gmail.com>
2021-12-18 22:05:07 +0600

    feature: analyze animal profile page done

--- src/components/Global/Badges/StatusBadge.vue
+++ src/components/Global/Badges/StatusBadge.vue
@@ -2,21 +2,34 @@
   <v-btn
     :color="getColor(status)"
     light
-    class="success--text text-none px-2"
+    :class="`${getTextColor(status)}--text text-none px-2`"
     width="auto"
     height="20px"
     depressed
   >
-    <span class="text-body-2">{{ status }}</span>
+    <span class="text-body-2">{{ text ? text : status }}</span>
   </v-btn>
 </template>
 
 <script lang="ts">
 export default {
-  props: ['status'],
+  props: ['status', 'text'],
   methods: {
     getColor(status) {
+      if (status === 'observe') {
+        return 'bg2'
+      } else if (status === 'sick') {
+        return 'error_bg'
+      }
       return 'success_bg'
+    },
+    getTextColor(status) {
+      if (status === 'observe') {
+        return 'accent'
+      } else if (status === 'sick') {
+        return 'error'
+      }
+      return 'success'
     },
   },
   name: 'StatusBadge',
---
+++ src/pages/Main/Animals/details/categories/analyze.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="d-flex justify-content-between analyze flex-wrap">
+    <v-card elevation="0" width="49%" class="mr-4 transparent mb-2 elevation-card">
+      <v-expansion-panels multiple flat accordion v-model="expandedPanels">
+        <v-expansion-panel>
+          <v-expansion-panel-header class="px-4">
+            <div>
+              <span class="gray_secondary--text">12.07.2021</span>
+              <h4 class="text-body-1 font-weight-bold my-2">Наименование диагноза</h4>
+              <StatusBadge :status="'observe'" :text="'Под наблюдением'" />
+            </div>
+          </v-expansion-panel-header>
+          <v-divider></v-divider>
+          <v-expansion-panel-content class="mt-2">
+            <PropertyWithLine
+              :values="[
+                {
+                  type: 'Предмет обследования',
+                  value:
+                    'Значимость этих проблем настолько очевидна, что постоянное информационно-пропагандистское обеспечение нашей деятельности требуют определения и уточнения модели развития',
+                },
+                {
+                  type: 'Лечение',
+                  links: [
+                    {
+                      name: 'Назначение №12345678',
+                      link: '/home/animals/AST_012124',
+                    },
+                  ],
+                  isLink: true,
+                },
+                {
+                  type: 'Лечение',
+                  links: [
+                    {
+                      name: 'Фамилия Имя',
+                      link: '/home/animals/AST_012124',
+                    },
+                  ],
+                  isLink: true,
+                },
+                {
+                  type: 'Пользователь',
+                  links: [
+                    {
+                      name: 'Фамилия Имя',
+                      link: '/home/animals/AST_012124',
+                    },
+                  ],
+                  isLink: true,
+                },
+                {
+                  type: 'Заметки',
+                  value:
+                    'Значимость этих проблем настолько очевидна, что постоянное информационно-пропагандистское обеспечение нашей деятельности требуют определения и уточнения модели развития',
+                },
+              ]"
+              typeSize="4"
+            >
+              <template v-slot:link="{ item }">
+                <div v-for="(obj, i) in item.links" :key="i" class="mb-1">
+                  <router-link
+                    :to="obj.link"
+                    class="accent--text text-decoration-underline"
+                    >{{ obj.name }}</router-link
+                  >
+                </div>
+              </template>
+            </PropertyWithLine>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+    <v-card elevation="0" width="49%" class="transparent">
+      <v-expansion-panels multiple accordion flat v-model="expandedPanels">
+        <v-expansion-panel>
+          <v-expansion-panel-header class="px-4">
+            <div>
+              <span class="gray_secondary--text">12.07.2021</span>
+              <h4 class="text-body-1 font-weight-bold my-2">Наименование диагноза</h4>
+              <StatusBadge :status="'sick'" :text="'Болен'" />
+            </div>
+          </v-expansion-panel-header>
+          <v-divider></v-divider>
+          <v-expansion-panel-content class="mt-2"> </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+    <v-card elevation="0" width="49%" class="transparent mr-4">
+      <v-expansion-panels multiple accordion flat v-model="expandedPanels">
+        <v-expansion-panel>
+          <v-expansion-panel-header class="px-4">
+            <div>
+              <span class="gray_secondary--text">12.07.2021</span>
+              <h4 class="text-body-1 font-weight-bold my-2">Наименование диагноза</h4>
+              <StatusBadge :status="'sick'" :text="'Болен'" />
+            </div>
+          </v-expansion-panel-header>
+          <v-divider></v-divider>
+          <v-expansion-panel-content class="mt-2"> </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+    <v-card elevation="0" width="49%" class="transparent">
+      <v-expansion-panels multiple accordion flat v-model="expandedPanels">
+        <v-expansion-panel>
+          <v-expansion-panel-header class="px-4">
+            <div>
+              <span class="gray_secondary--text">12.07.2021</span>
+              <h4 class="text-body-1 font-weight-bold my-2">Наименование диагноза</h4>
+              <StatusBadge :status="'observe'" :text="'Под наблюдением'" />
+            </div>
+          </v-expansion-panel-header>
+          <v-divider></v-divider>
+          <v-expansion-panel-content class="mt-2"> </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { descent } from './index'
+import { mdiInformationOutline } from '@mdi/js'
+import { ref } from '@vue/composition-api'
+export default {
+  name: 'analyze',
+  setup() {
+    const decent = descent()
+    const cards = decent.cards
+    const expandedPanels = ref(Array.from({ length: 2 }, (v, k) => k))
+    return {
+      cards,
+      mdiInformationOutline,
+      expandedPanels,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.analyze .v-expansion-panel-header__icon {
+  align-self: start;
+}
+</style>
